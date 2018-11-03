9c6444bfc1344f92197ac61172a84c74039acb46
Wasif Khan <waasif.khaan@gmail.com>
2018-11-03 21:19:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,12 +4,7 @@ function driversWithRevenueOver(drivers,revenue){
 }
 // [1, 2, 3, 4, 5].filter(function (num) { return num > 3; });
 // // => [4, 5]
-function driverNamesWithRevenueOver(drivers,revenue){
-  return driversWithRevenueOver(drivers,revenue).map(function (driver){
-    return driver.name;
-  }
-)}
-}
+
 
 function exactMatch(drivers,{key: value}){
   return drivers.map(function(driver){
