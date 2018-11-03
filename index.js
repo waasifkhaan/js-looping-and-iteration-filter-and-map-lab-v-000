53f1e78e1d4801d54a8613d3379b2e93350bdb5d
Wasif Khan <waasif.khaan@gmail.com>
2018-11-03 21:28:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,14 +19,4 @@ function exactMatch(drivers,matcher){
     }
   })
 }
-function exactMatch (drivers, matcher) {
-  return drivers.filter(function (driver) {
-    let matches = false;
 
-    for (const key in matcher) {
-      matches = driver[key] === matcher[key];
-    }
-
-    return matches;
-  });
-}
