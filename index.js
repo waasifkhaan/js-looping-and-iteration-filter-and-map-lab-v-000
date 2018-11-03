534ce358c1c3dc719db8a0b67f2f2db7a0c4e344
Wasif Khan <waasif.khaan@gmail.com>
2018-11-03 21:04:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 function driversWithRevenueOver(drivers,revenue){
-  return drivers.filter(function(driver){return driver => driver.revenue > revenue })
+  return drivers.filter(function(driver){
+    return driver.revenue > revenue })
 }
+[1, 2, 3, 4, 5].filter(function (num) { return num > 3; });
+// => [4, 5]
